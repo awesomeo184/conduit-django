15e83c6c3301699af91ca4c216d1b52718ccda11
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-05-08 18:47:57 +0900

    feat: add RegisterationSerializer

---
+++ conduit/apps/authentication/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializers registration requests and creates a new user."""
+
+    # Ensure passwords are at least 8 characters long, no longer than 128
+    # characters, and can not be read by the client.
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    # The client should not be able to send a token along with a registration
+    # request. Making `token` read-only handles that for us.
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        # List all of the fields that could possibly be included in a request
+        # or response, including fields specified explicitly above.
+        fields = ['email', 'username', 'password', 'token']
+
+    def create(self, validated_data):
+        # 정의 해주지 않으면 DRF의 디폴트 메서드가 사용된다.
+        return User.objects.create_user(**validated_data)
