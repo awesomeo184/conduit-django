e78a92a70716db39d084405f84864e5a837a8af9
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-05-15 18:58:57 +0900

    feat: add LoginSerializer

--- conduit/apps/authentication/serializers.py
+++ conduit/apps/authentication/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 from .models import User
@@ -27,3 +28,52 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # 정의 해주지 않으면 DRF의 디폴트 메서드가 사용된다.
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    username = serializers.CharField(max_length=255, read_only=True)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError(
+                'An email address is required to log in.'
+            )
+        if password is None:
+            raise serializers.ValidationError(
+                'A password is required to log in.'
+            )
+
+        # The `authenticate` method is provided by Django and handles checking
+        # for a user that matches this email/password combination. Notice how
+        # we pass `email` as the `username` value since in our User
+        # model we set `USERNAME_FIELD` as `email`.
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password was not found.'
+            )
+
+        # Django provides a flag on our `User` model called `is_active`. The
+        # purpose of this flag is to tell us whether the user has been banned
+        # or deactivated. This will almost never be the case, but
+        # it is worth checking. Raise an exception in this case.
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'This user has been deactivated.'
+            )
+
+        # The `validate` method should return a dictionary of validated data.
+        # This is the data that is passed to the `create` and `update` methods
+        # that we will see later on.
+        return {
+            'email': user.email,
+            'username': user.username,
+            'token': user.token
+        }
